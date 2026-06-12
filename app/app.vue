1fd15fd73944c0ecffd76b15697121ed6aaa0f31
“Zein-s” <zainurroziqin3010@gmail.com>
2026-06-12 20:25:35 +0700

    update judul / titel bar  ormawa ut surabaya

--- app/app.vue
+++ app/app.vue
@@ -4,3 +4,15 @@
     <NuxtPage />
   </div>
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: 'Ormawa UT Surabaya',
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} | Ormawa UT Surabaya` : 'Ormawa UT Surabaya';
+  },
+  link: [
+    { rel: 'icon', type: 'image/jpeg', href: '/img/logo ormawa bulat.jpg' }
+  ]
+})
+</script>
